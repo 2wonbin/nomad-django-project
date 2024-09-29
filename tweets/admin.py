ed34e1e03342bf7e8c326c259e56cbfcb4415ff1
이원빈 <2wonbin@kakao.com>
2024-09-30 00:53:05 +0900

    tweet app 생성 및 model, admin 설정

---
+++ tweets/admin.py
@@ -0,0 +1,55 @@
+from django.contrib import admin
+
+from .models import Tweet, Like
+
+
+@admin.register(Tweet)
+class TweetAdmin(admin.ModelAdmin):
+
+    fieldsets = (
+        (
+            "Tweet",
+            {
+                "fields": (
+                    "payload",
+                    "user",
+                ),
+                "classes": ("wide",),
+            },
+        ),
+        (
+            "Dates",
+            {
+                "fields": ("created_at", "updated_at"),
+                "classes": ("collapse",),
+            },
+        ),
+    )
+    readonly_fields = ("created_at", "updated_at")
+    list_display = ("payload", "user")
+
+
+@admin.register(Like)
+class LikeAdmin(admin.ModelAdmin):
+
+    fieldsets = (
+        (
+            "Like",
+            {
+                "fields": (
+                    "user",
+                    "tweet",
+                ),
+                "classes": ("wide",),
+            },
+        ),
+        (
+            "Dates",
+            {
+                "fields": ("created_at", "updated_at"),
+                "classes": ("collapse",),
+            },
+        ),
+    )
+    readonly_fields = ("created_at", "updated_at")
+    list_display = ("user", "tweet")
---
+++ tweets/models.py
@@ -0,0 +1,20 @@
+from django.db import models
+
+from users.models import User
+from common.models import CommonModel
+
+
+class Tweet(CommonModel):
+    payload = models.TextField(max_length=180)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.payload
+
+
+class Like(CommonModel):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    tweet = models.ForeignKey(Tweet, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"{self.user} likes {self.tweet}"
