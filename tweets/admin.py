5bdf6f52e2267f2ddf8409d4662d91345f790fd1
이원빈 <2wonbin@kakao.com>
2024-10-02 00:45:27 +0900

    tweet, like 모델 admin 개선

--- tweets/admin.py
+++ tweets/admin.py
@@ -3,6 +3,20 @@ from django.contrib import admin
 from .models import Tweet, Like
 
 
+class WordFilter(admin.SimpleListFilter):
+    title = "'Elon Musk' 키워드 포함 여부"
+
+    parameter_name = "is_contain_elon_musk"
+
+    def lookups(self, request, model_admin):
+        return (("true", "Yes"),)
+
+    def queryset(self, request, tweets):
+        if self.value() == "true":
+            return tweets.filter(payload__icontains="Elon Musk")
+        return tweets
+
+
 @admin.register(Tweet)
 class TweetAdmin(admin.ModelAdmin):
 
@@ -27,6 +41,12 @@ class TweetAdmin(admin.ModelAdmin):
     )
     readonly_fields = ("created_at", "updated_at")
     list_display = ("payload", "user", "get_like_count")
+
+    search_fields = ("payload", "user__username")
+    list_filter = (
+        WordFilter,
+        "created_at",
+    )
 
 
 @admin.register(Like)
@@ -53,3 +73,5 @@ class LikeAdmin(admin.ModelAdmin):
     )
     readonly_fields = ("created_at", "updated_at")
     list_display = ("user", "tweet")
+    search_fields = ("user__username",)
+    list_filter = ("created_at",)
